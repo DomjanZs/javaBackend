e832d3aaf6ff8eb8e200878fba85bd9f3ac4c9f7
domjanzsombor-admin <domjanzsom@gmail.com>
2024-07-24 02:26:14 +0200

    resolve some minor issue

--- src/main/java/org/example/config/PathConfig.java
+++ src/main/java/org/example/config/PathConfig.java
@@ -1,17 +1,15 @@
 package org.example.config;
 
-import java.io.File;
-
 public class PathConfig {
-    public static final String BASE_PATH = new File(".").getAbsolutePath();
+    public static final String BASE_PATH = "./javaBackend";
     public static final String LOGS_PATH = BASE_PATH + "/src/logs";
     public static final String DATA_PATH = BASE_PATH + "/data";
     public static final String REPORTS_PATH = BASE_PATH + "/reports";
 
     public static final String APPLICATION_LOG_PATH = LOGS_PATH + "/application.log";
-    public static final String CUSTOMER_FILE_PATH = "data/customer.csv";
-    public static final String PAYMENTS_FILE_PATH = "data/payments.csv";
-    public static final String CUSTOMER_PURCHASE_REPORT = "reports/report01.csv";
-    public static final String TOP_SPENDERS_REPORT_PATH = "reports/top.csv";
-    public static final String WEB_SHOP_REVENUE_REPORT_PATH = "reports/report02.csv";
+    public static final String CUSTOMER_FILE_PATH = DATA_PATH + "/customer.csv";
+    public static final String PAYMENTS_FILE_PATH = DATA_PATH + "/payments.csv";
+    public static final String CUSTOMER_PURCHASE_REPORT = REPORTS_PATH + "/report01.csv";
+    public static final String TOP_SPENDERS_REPORT_PATH = REPORTS_PATH + "/top.csv";
+    public static final String WEB_SHOP_REVENUE_REPORT_PATH = REPORTS_PATH + "/report02.csv";
 }
